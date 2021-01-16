fd3afe033d32cbc0ee23d091b9cd7c08c85a7545
svasandani <vasandanishailesh@gmail.com>
2021-01-16 12:11:39 -0500

    search by job

--- keywords.js
+++ keywords.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 
-var text = fs.readFileSync("top1000descriptions-2").toString('utf-8');
+let text = fs.readFileSync("descriptions.json").toString('utf-8');
+
+let textobj = JSON.parse(text);
 
 const programmingLanguages = ['Bash', 'C', 'C++', "C#", "CSS", 'Dreamweaver', 'Kotlin',  "Go", 'Golang',  'HTML', "Javascript", "Java", "Node" , "MySQL", "Objective-C", 'python', 'PERL', 'php', 'qt', 'sql', 'R', 'ruby', 'Ruby on Rails', 'Scala', 'Smalltalk', 'mySQL', 'postgres', 'postgresql', 'graphql', 'mongodb', 'Swift', 'jQuery', 'Matlab', 'React', 'aws', 'typescript', ];
-const languagesData = new Map();
-// D
+const languagesData = {};
+const jobData = [];
 
 programmingLanguages.map((language) => {
-  languagesData.set(language, 0);
+  languagesData[language] = 0;
 })
 
 //C: case sensitive
@@ -16,15 +18,36 @@ function escapeRegExp(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
 
-programmingLanguages.forEach((language, i) => {
-  let regex = new RegExp(`(?:\\s|\\W|>)(${escapeRegExp(language)})(?:\\s|\\W|<)`, 'gmi');
+textobj.forEach(job => {
+  let langs = {};
 
-  if (language === 'C') regex = new RegExp(`(?:\\s|\\W|>)(C)(?:\\s|,|\\\\.|\\/|&|<)`, 'gmi');
+  programmingLanguages.forEach((language, i) => {
+    let regex = new RegExp(`(?:\\s|\\W|>)(${escapeRegExp(language)})(?:\\s|\\W|<)`, 'gmi');
+  
+    if (language === 'C') regex = new RegExp(`(?:\\s|\\W|>)(C)(?:\\s|,|\\\\.|\\/|&|<)`, 'gmi');
 
-  console.log(text.match(regex));
+    let matches = job.description.match(regex);
+  
+    langs[language] = matches ? matches.length : 0;
+    languagesData[language] += matches ? matches.length : 0;
+  });
 
-  languagesData.set(language, text.match(regex)?.length);
+  job.keywords = langs;
+
+  jobData.push(job);
 })
 
-console.log(languagesData)
+// programmingLanguages.forEach((language, i) => {
+//   let regex = new RegExp(`(?:\\s|\\W|>)(${escapeRegExp(language)})(?:\\s|\\W|<)`, 'gmi');
 
+//   if (language === 'C') regex = new RegExp(`(?:\\s|\\W|>)(C)(?:\\s|,|\\\\.|\\/|&|<)`, 'gmi');
+
+//   console.log(text.match(regex));
+
+//   languagesData.set(language, text.match(regex)?.length);
+// })
+
+console.log(languagesData);
+
+fs.writeFileSync('languagesData.json', JSON.stringify(languagesData));
+fs.writeFileSync('jobData.json', JSON.stringify(jobData));
