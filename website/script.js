1219b0e1d696a19f7c74472951411126ddf61be1
Rebecca <rebecca_517@hotmail.com>
2021-01-16 16:04:21 -0500

    truncating xlabels

--- website/script.js
+++ website/script.js
@@ -143,31 +143,35 @@ const buildTopKeywordsGraph = (resultEl, data) => {
   y.domain([0, d3.max(data, function(d) { return parseFloat(d.percent); })]);
 
   let svg = d3.select('.visualization-container:not(.selected)')
-  .attr('class', 'visualization-container selected')
-  .append('svg')
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
+    .attr('class', 'visualization-container selected')
+    .append('svg')
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
     .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
   svg.selectAll(".bar")
-      .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.language); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(d.percent); })
-        .attr("height", function(d) { return height - y(parseFloat(d.percent)); });
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.language); })
+    .attr("width", x.bandwidth())
+    .attr("y", function(d) { return y(d.percent); })
+    .attr("height", function(d) { return height - y(parseFloat(d.percent)); });
 
   // add the x Axis
   svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
   // add the y Axis
   svg.append("g")
       .call(d3.axisLeft(y));
+}
+
+const truncateLabel = (arr) => {
+  
 }
 
 const buildTopCompaniesGraph = (resultEl, data) => {
@@ -183,7 +187,7 @@ const buildTopCompaniesGraph = (resultEl, data) => {
   resultEl.appendChild(tk);
 
   // set the dimensions and margins of the graph
-  let margin = {top: 20, right: 20, bottom: 30, left: 40},
+  let margin = {top: 20, right: 20, bottom: 50, left: 40},
   width = 480 - margin.left - margin.right,
   height = 240 - margin.top - margin.bottom;
 
@@ -193,39 +197,53 @@ const buildTopCompaniesGraph = (resultEl, data) => {
         .padding(0.3);
   let y = d3.scaleLinear()
         .range([height, 0]);
-  let xAxis = d3.svg.axis()
-  .scale(x);
+  // let xAxis = d3.svg.axis()
+  // .scale(x);
 
-  x.domain(data.map(function(d) { return d.company; }));
+  x.domain(data.map(function(d) {
+    let xLabel;
+    if (d.company.length <= 8) {
+      xLabel = d.company
+    } else {
+      xLabel = d.company.slice(0,8).concat('..')
+    }
+     return xLabel; 
+  }));
   y.domain([0, d3.max(data, function(d) { return parseFloat(d.count); })]);
 
   let svg = d3.select('.visualization-container:not(.selected)')
-  .attr('class', 'visualization-container selected')
-  .append('svg')
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
+    .attr('class', 'visualization-container selected')
+    .append('svg')
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
     .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
   svg.selectAll(".bar")
-      .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        
-        .attr("x", function(d) { return x(d.company); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(d.count); })
-        .attr("height", function(d) { return height - y(parseFloat(d.count)); });
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.company); })
+    .attr("width", x.bandwidth())
+    .attr("y", function(d) { return y(d.count); })
+    .attr("height", function(d) { return height - y(parseFloat(d.count)); });
 
   // add the x Axis
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x))
+      .selectAll("text")  
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", ".15em")
+          .attr("transform", "rotate(-40)" );
+      // .call(xAxis)
+      // .attr("transform", "rotate(90)")
 
   // add the y Axis
   svg.append("g")
-      .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y));
 }
 
 async function fetchAndBuildResults(resultEl, query) {
