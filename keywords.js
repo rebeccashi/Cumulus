82679e8da4e135b4238e153cf1f65f4ba5fe074e
Rebecca <rebecca_517@hotmail.com>
2021-01-15 23:12:18 -0500

    buggy string parse

--- keywords.js
+++ keywords.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-var text = fs.readFileSync("top1000descriptions").toString('utf-8');
+var text = fs.readFileSync("top1000descriptions-2").toString('utf-8');
 
 const programmingLanguages = ['Bash', 'C', 'C++', "C#", "CSS", 'Dreamweaver', 'Kotlin',  "Go", 'Golang',  'HTML', "Javascript", "Java", "Node.js" , "MySQL", "Objective-C", 'python', 'PERL', 'php', 'qt', 'sql', 'R', 'Ruby on Rails', 'Scala', 'Smalltalk', 'SQL', 'Swift', 'jQuery', 'Matlab'];
 const languagesData = [];
@@ -16,15 +16,37 @@ programmingLanguages.map( (language) => {
 
 //C: case sensitive
 
-programmingLanguages.forEach ((language, i) => {
+programmingLanguages.forEach((language, i) => {
     var loop = true;
-    let index = 0
-    while (loop) {
-        index = text.indexOf(language.concat(','), index)
-        console.log(`${language}: ${index}`)
-        if (index == -1) break;
-        index += 1
-        languagesData[i].count++;
+    let index = 0, nextChar = '';
+    //case sensitive
+    if (language == 'C' || language == 'C++') {
+        while (loop) {
+            index = text.indexOf(language, index)   
+            if (index == -1) break;
+            nextChar = text.slice(index+1, index+2);
+            // console.log(nextChar)
+            if (nextChar == ' ' || nextChar == ',' || nextChar == ':') {
+                console.log(`${language}: ${index}`)
+                languagesData[i].count++;
+            }
+            index += 1;
+        }
+    //case insensitive
+    } else {
+        let stringCopy = text.repeat(1) 
+        while (loop) {
+            index = stringCopy.search(new RegExp(language, "i"))
+            if (index == -1) break;
+            nextChar = text.slice(index+1, index+2);
+            stringCopy = stringCopy.slice(index + 1);
+
+            if (nextChar == ' ' || nextChar == ',' || nextChar == ':') {
+                console.log(`${language}: ${index}`)
+                languagesData[i].count++;
+            }
+            console.log(index);
+        }
     }
 })
 
