b3199ddc77792bf5c9f5ad22dc096fe9bd64ca28
Rebecca <rebecca_517@hotmail.com>
2021-03-14 16:15:36 -0400

    react router dom + routing setup

--- react-site/src/App.js
+++ react-site/src/App.js
@@ -1,10 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Landing from './Landing/Landing'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Cumulus</Link>
+              </li>
+              <li>
+                <Link to="/users">Pricing</Link>
+              </li>
+              <li>
+                <Link to="/about">About us</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/">
+              <Landing />
+            </Route>
+
+            {/* <Route path="/pricing">
+              <Pricing />
+            </Route>
+          */}
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ react-site/src/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Landing extends React.Component {
+
+    render() {
+        return (
+            <p>Landing</p>
+        )
+    }
+
+}
+
+export default Landing;
