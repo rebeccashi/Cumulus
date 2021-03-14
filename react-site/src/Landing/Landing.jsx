d73d8577499fcd27eaad496dc5385f4c51f96382
Rebecca <rebecca_517@hotmail.com>
2021-03-14 19:46:27 -0400

    add search bar

--- react-site/src/Landing/Landing.jsx
+++ react-site/src/Landing/Landing.jsx
@@ -9,6 +9,10 @@ class Landing extends React.Component {
                 <div className="tagline">
                     <h1 className="title">Get a bird's-eye view of your field</h1>
                     <h2 className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus, est aliquam imperdiet consectetur, purus odio pulvinar orci, ut volutpat ex justo dapibus metus. </h2>
+                    <div id="search-bar">
+                        <span id="search-bar-text">Job title, keywords, company or location</span>
+                        <div id="search-button">Search</div>
+                    </div>
                 </div>
             </div>
         )
