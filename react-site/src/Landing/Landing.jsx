12b47be7f006f695522e1735a5d6edc5e538786f
Rebecca <rebecca_517@hotmail.com>
2021-03-15 00:00:18 -0400

    clouds svg wip

--- react-site/src/Landing/Landing.jsx
+++ react-site/src/Landing/Landing.jsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import './Landing.css';
+import Cloud1 from '../images/cloud1.svg'
 
 class Landing extends React.Component {
 
     render() {
         return (
             <div className="landing">
+                <img src={Cloud1} className="cloud" id="cloud1"/>
+                {/* <svg>
+                    <use id="cloud1" href="abstract-clouds.svg"/>
+                </svg> */}
                 <div className="tagline">
                     <h1 className="title">Get a bird's-eye view of your field</h1>
                     <h2 className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus, est aliquam imperdiet consectetur, purus odio pulvinar orci, ut volutpat ex justo dapibus metus. </h2>
