8e603646f107fc23092cf62c033aa56ead019043
Rebecca <rebecca_517@hotmail.com>
2021-03-15 10:49:14 -0400

    use grid on landing

--- react-site/src/Landing/Landing.jsx
+++ react-site/src/Landing/Landing.jsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import './Landing.css';
-import Cloud1 from '../images/cloud1.svg'
+import Cloud1 from '../images/cloud1.svg';
+import Cloud2 from '../images/cloud2.svg';
+import Cloud3 from '../images/cloud3.svg';
+import Cloud4 from '../images/cloud4.svg';
+import Cloud5 from '../images/cloud5.svg';
 
 class Landing extends React.Component {
 
@@ -8,9 +12,11 @@ class Landing extends React.Component {
         return (
             <div className="landing">
                 <img src={Cloud1} className="cloud" id="cloud1"/>
-                {/* <svg>
-                    <use id="cloud1" href="abstract-clouds.svg"/>
-                </svg> */}
+                <img src={Cloud2} className="cloud" id="cloud2"/>
+                <img src={Cloud3} className="cloud" id="cloud3"/>
+                <img src={Cloud4} className="cloud" id="cloud4"/>
+                <img src={Cloud5} className="cloud" id="cloud5"/>
+                
                 <div className="tagline">
                     <h1 className="title">Get a bird's-eye view of your field</h1>
                     <h2 className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus, est aliquam imperdiet consectetur, purus odio pulvinar orci, ut volutpat ex justo dapibus metus. </h2>
