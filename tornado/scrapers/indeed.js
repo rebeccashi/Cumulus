d0364aa4af6a3bff64d2b9c25180d26f2af78e32
svasandani <vasandanishailesh@gmail.com>
2021-11-02 16:21:19 -0400

    Add job parsing from search results

--- tornado/scrapers/indeed.js
+++ tornado/scrapers/indeed.js
@@ -43,21 +43,97 @@ class IndeedQuery {
   // update this if UI changes (cheerio/scraping)
   scrapePageForJob(html) {
     const $ = cheerio.load(html);
+    const jobTable = $("#resultsCol");
+    const jobs = jobTable.find(".result");
+
+    const jobObjects = jobs
+    .map((i, e) => {
+      const job = $(e);
+
+      const jobtitle = job
+        .find(".jobTitle > span")
+        .text()
+        .trim();
+
+      const url = "https://indeed.com" + job.attr("href");
+
+      const summary = job
+        .find(".job-snippet")
+        .text()
+        .trim();
+
+      const company =
+        job
+          .find(".companyName")
+          .text()
+          .trim() || null;
+
+      const location = job
+        .find(".companyLocation")
+        .text()
+        .trim();
+
+      const postDate = job
+        .find(".date")
+        .text()
+        .trim();
+
+      const salary = job
+        .find(".salary-snippet")
+        .text()
+        .trim();
+
+      const isEasyApply =
+        job
+          .find(".ialbl")
+          .text()
+          .trim() === "Easily apply";
+
+      return {
+        title: jobtitle,
+        summary: summary,
+        url: url,
+        company: company,
+        location: location,
+        postDate: postDate,
+        salary: salary,
+        isEasyApply: isEasyApply
+      };
+    })
+
+    return jobObjects;
   }
 
   // we use a recursive strategy to get every page
-  recursivelyGetJobPage(jobData, keyword) {
+  recursivelyGetJobPage(jobData) {
     fetch(this.url(), {
+      // TODO: run through new list of proxies
       // agent: new HttpsProxyAgent('http://152.179.12.86:3128')
     })
+    // TODO: check for errors here, add breaking case
     .then(response => response.text())
     .then(data => {
-      console.log(data);
-    });
+      const newJobs = this.scrapePageForJob(data);
+      if (typeof newJobs === 'undefined') throw new Error("Undefined list of new jobs!");
+      if (jobData.length > 10) throw new Error("Lots of jobs");
+
+      this.start += newJobs.length;
+      jobData = [...jobData, ...newJobs];
+
+      console.log(`Scraped the next ${newJobs.length} jobs!`)
+      
+      this.recursivelyGetJobPage(jobData);
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(jobData);
+      return jobData;
+    })
+
   }
 
   getAllJobs() {
-    return this.recursivelyGetJobPage({}, this.keyword)
+    return this.recursivelyGetJobPage([], this.keyword)
   }
 }
 
@@ -67,6 +143,7 @@ const getAllJobsForKeyword = (keyword) => {
   })
 
   const jobs = iq.getAllJobs();
+  console.log(jobs);
 }
 
 getAllJobsForKeyword("software engineer");
