3f62e65e14ba32ba3907a6d2931ffd3bb5079100
svasandani <vasandanishailesh@gmail.com>
2021-11-06 13:25:13 -0400

    Add retry to fetch

--- tornado/src/js_scrapers/emsi.js
+++ tornado/src/js_scrapers/emsi.js
@@ -9,6 +9,25 @@ const sleep = (ms) => {
   });
 };
 
+const retry = async (callback, count, description) => {
+  console.log(`(Re)trying function: ${description}...`);
+
+  try {
+    return await callback();
+  } catch (err) {
+    console.log(`Function ${description} failed with ${count} tries left!`)
+    if (count <= 0) {
+      console.log(`Throwing this error to caller!`)
+      throw err;
+    }
+
+    await sleep(1000);
+    
+    console.log(`Trying again now...`)
+    return await retry(callback, count - 1);
+  }
+}
+
 console.log("Scraping EMSI!");
 
 class EmsiQuery {
@@ -182,7 +201,7 @@ class EmsiQuery {
 
     const url = this.remainingSkills.get(skill);
 
-    await this.page.goto(url, { waitUntil: "networkidle0" });
+    await retry(() => this.page.goto(url, { waitUntil: "networkidle0" }), 10, `Get skill ${skill}`)
 
     const data = await this.scrapeHTMLForSkill();
 
