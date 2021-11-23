92e592ddff3e5074f8d495822926f8b932181bfa
svasandani <vasandanishailesh@gmail.com>
2021-11-22 20:44:39 -0500

    Add Input component

---
+++ react-site/src/components/Input/Input.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import Icon from '../Icon';
+
+import './Input.css';
+
+export const Input = ({
+  color='blue',
+  placeholder,
+  label,
+  withIcon,
+  iconVariant,
+  setValue
+}) => {
+  const colorClass = `input-color--${color}`;
+
+  const [isActive, setIsActive] = React.useState(false);
+  
+  return (
+    <div class={`input ${isActive ? 'active' : ''}`}>
+      <span className='input-placeholder'>{
+        isActive ?
+        label :
+        placeholder
+      }</span>
+      <input 
+        type='text' 
+        className={`input-input ${colorClass} ${withIcon ? 'with-icon' : ''}`} 
+        onFocus={() => setIsActive(true)}
+        onBlur={(e) => {
+          if (e.target.value.length == 0) setIsActive(false)
+        }}
+        onChange={(e) => {
+          setValue(e.target.value);
+          if (e.target.value.length == 0) setIsActive(false)
+          else setIsActive(true)
+        }}
+      />
+      {
+        withIcon ?
+        (
+          <div  className='input-icon'>
+            <Icon variant={iconVariant} color='purple' size='xs' />
+          </div> 
+        ) : 
+        null
+      }
+    </div>
+  );
+}
