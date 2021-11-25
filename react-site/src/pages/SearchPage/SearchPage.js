1e24eeb13249f79457bad53505ca754af8e77acc
svasandani <vasandanishailesh@gmail.com>
2021-11-24 20:12:49 -0500

    Update search page logic

--- react-site/src/pages/SearchPage/SearchPage.js
+++ react-site/src/pages/SearchPage/SearchPage.js
@@ -7,8 +7,69 @@ import Heading from '../../components/Heading';
 import Text from '../../components/Text';
 
 export const SearchPage = ({ searchValue }) => {
+  const [autocomplete, setAutocomplete] = React.useState('');
+  const [query, setQuery] = React.useState('');
+  const [selectedObject, setSelectedObject] = React.useState(null);
+
+  React.useEffect(() => {
+    if (searchValue && searchValue.length > 0) {
+      setQuery(searchValue);
+    }
+  }, [searchValue])
 
   return (
-    <>{searchValue}</>
+    <>
+      <div className='search'>
+        <div className='sidebar'>
+          <Heading variant='h2'>
+            /search
+          </Heading>
+          <Input 
+            autofocus={true}
+            autocomplete={autocomplete}
+            placeholder='Job title, keywords, company, or location'
+            withIcon={true} 
+            iconVariant='search' 
+            value={query}
+            setValue={(newValue) => {
+              setQuery(newValue)
+            }}
+            style={{
+              width: '100%'
+            }}
+          />
+          <br />
+          <Text>
+            Auto-generated suggestions will appear here throughout your search.
+          </Text>
+        </div>
+        <div className='main'>
+          {
+            selectedObject == null ?
+            (
+              query === '' ?
+              (
+                <>
+                  <Heading variant='h1'>Always helpful</Heading>
+                  <Text>
+                    As you type, we'll display a few results that match.
+                  </Text>
+                </>
+              ) :
+              (
+                <>
+                  results for {query}
+                </>
+              )
+            ) :
+            (
+              <>
+                object selected {selectedObject}
+              </>
+            )
+          }
+        </div>
+      </div>
+    </>
   )
 }
