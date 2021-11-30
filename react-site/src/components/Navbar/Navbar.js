ba0dd40bda253c19fb84d2517d2cc00aa8378390
svasandani <vasandanishailesh@gmail.com>
2021-11-29 23:19:34 -0500

    Add navbar links and improve styling

--- react-site/src/components/Navbar/Navbar.js
+++ react-site/src/components/Navbar/Navbar.js
@@ -5,13 +5,18 @@ import "./Navbar.css";
 
 import Button from "../Button";
 import Heading from "../Heading";
+import Text from "../Text";
 
 export const Navbar = ({ signedIn = false }) => {
   return (
     <nav className="navbar">
-      <Link to="/" className="logo">
-        <Heading variant="cta">cumulus</Heading>
-      </Link>
+      <div className="logo">
+        <Link to="/">
+          <Heading variant="cta">cumulus</Heading>
+        </Link>
+      </div>
+      <Text>Pricing</Text>
+      <Text>About Us</Text>
       {signedIn ? null : (
         <div className="cta-button">
           <Button color="cta" label="Sign Up" />
