23fcbe2ad33da337a94468bd0a4173a9c2364414
ad4565 <54941861+ad4565@users.noreply.github.com>
2021-12-01 09:48:49 -0500

    Changed data formatting

--- lightning/index.js
+++ lightning/index.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const cors = require('cors');
 const app = express();
 const {MongoClient} = require('mongodb');
+const dotEnv = require('dotenv').config()
 app.use(express.json());
 app.use(cors());
 
 //MongoDB connection string
-const uri = "mongodb+srv://cumulus:headintheclouds@jobs.tcmdh.mongodb.net/jobs?retryWrites=true&w=majority"
+const uri = "mongodb+srv://" + process.env.REACT_APP_MONGO_DB_ID + ":" + process.env.REACT_APP_MONGO_DB_SECRET + "@jobs.tcmdh.mongodb.net/jobs?retryWrites=true&w=majority"
 const client = new MongoClient(uri);
 
 // pretend reading from file system is like a database lol
@@ -28,10 +29,32 @@ Object.keys(keywords).forEach(l => {
 
 
 
-  var results = '{"autocomplete": "", "results": []}';
+
+  
 
 
- async function searchCompanies() {
+
+
+
+
+
+
+
+
+
+/*
+  { "query": "software engineering internship new york" }
+*/
+app.get('/api/search', (req, res) => {
+  if (!req.query.q) {
+    res.status(406).send('Missing data');
+    return;
+  }
+
+  var results = '{"autocomplete": "", "results": []}'
+
+
+ async function search() {
     try{
 
       //connect to mongo
@@ -39,30 +62,38 @@ Object.keys(keywords).forEach(l => {
 
       var jsonObj = JSON.parse(results);
 
+      //grab results 
   var cursor = client.db("jobs").collection("companies").find({
-    "name" : { $regex: "d"}
+    "name" : { $regex: req.query.q}
   });
 
   const companies = await cursor.toArray();
-
+  //loop through results
   if(companies.length > 0 ){
     companies.forEach((result, i) => {
+
+      var listings = 0;
+      //add up total listings
+      for(var i = 0; i < result.copies[0].skills.length; i++){
+        //.replace gets ride of seperators between the thousands i.e 1,234 -> 1234
+      listings = listings + Number(result.copies[0].skills[i].numJobs.replace(',', ''));      
+    }
 
       company = {
         "_id": result._id,
         "name": result.name,
         "category": "Company",
-        "listings": result.copies[0]
+        "listings": listings
 
       }
-
+      //adds results to jsonObj
       jsonObj['results'].push(company);
     });
   }
 
 
      cursor = client.db("jobs").collection("skills").find({
-      "name" : {$regex: "d"}
+      "name" : {$regex: req.query.q}
     });
     const skills = await cursor.toArray();
 
@@ -82,7 +113,7 @@ Object.keys(keywords).forEach(l => {
      }
 
      cursor = client.db("jobs").collection("titles").find({
-      "name" :{$regex: "d"}
+      "name" :{$regex: req.query.q}
      });
 
      const titles = await cursor.toArray();
@@ -90,11 +121,18 @@ Object.keys(keywords).forEach(l => {
      if(titles.length > 0 ){
       titles.forEach((result, i ) =>{
 
+        var listings = 0;
+
+        for(var  i = 0; i < result.copies[0].skills.length; i ++){
+          listings = listings + Number(result.copies[0].skills[i].numJobs.replace(',', ''));      
+
+        }
+
         title = {
           "_id": result._id,
           "name": result.name,
           "category": "Title",
-          "listings": result.copies[0]
+          "listings": listings
         }
 
         jsonObj["results"].push(title);
@@ -119,100 +157,7 @@ Object.keys(keywords).forEach(l => {
 
 
 
-searchCompanies().then(res => console.log(res));
-
-
-
-
-
-
-
-
-
-/*
-  { "query": "software engineering internship new york" }
-*/
-app.get('/api/search', (req, res) => {
-  if (!req.query.q) {
-    res.status(406).send('Missing data');
-    return;
-  }
-
-  var results = '{"autocomplete": "", "results": []}'
-
-
- async function searchCompanies() {
-    try{
-
-      var jsonObj = JSON.parse(results);
-
-  const cursor = client.db("jobs").collection("companies").find({
-    "name" : { $regex: req.query.q}
-  })
-
-  const companies = await cursor.toArray();
-
-  if(companies.length > 0 ){
-    companies.forEach((result, i) => {
-
-      company = {
-        "_id": result._id,
-        "name": result.name,
-        "category": "Company",
-        "listings": result.copies[0]
-
-      }
-
-      jsonObj['results'].push(company);
-    });
-    results = JSON.stringify(jsonObj);
-  }
- }
-
- catch (error){
-  console.log(error);
- }
-  
-}
-
-async function searchSkills(){
-  try{
-
-
-    var jsonObj = JSON.parse(results);
-
-
-    const cursor = client.db("jobs").collection("skills").find({
-      "name" : {$regex: req.query.q}
-    })
-
-    const skills = await cursor.toArray();
-
-    
-    if(skills.length > 0 ){
-      skills.forEach((result, i) => {
-
-        skill = {
-          "_id" : result._id,
-          "name": result.name,
-          "category": "Skill",
-          "listings": result.copies[0].numJobs
-        }
-
-        jsonObj["results"].push(skill);
-      });
-      results = JSON.stringify(jsonObj);
-     }
-
-  }
-
-  catch(error){
-    console.log(error);
-  }
-}
-
-searchCompanies();
-searchSkills();
+search().then(results => res.stauts(200).send(results));
 });
 
 /*
